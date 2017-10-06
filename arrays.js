3fbee3e772afcf3bbb21c4ac90b25b7e7bbc2828
Cheapo587 <ac.shultz587@gmail.com>
2017-10-06 17:50:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles",
+]
+
+chocolateBars.push("butter fingers")
+
+chocolateBars.unshift("heath")
+
+function addElementToBeginningOfArray(arrayOne, element){
+  var arrayTwo = [element, ...arrayOne]
+  return arrayTwo
+}
+
+function desstructivelyAddElementToBeginningOfArray(arrayOne, element){
+  arrayOne = [element, ...arrayOne]
+  return arrayOne
+}
+
+function addElementToEndOfArray(arrayOne, element){
+  arrayTwo = [...arrayOne, element]
+  return arrayTwo
+}
+
+function destructivelyAddElementToEndOfArray(arrayOne, element){
+  arrayOne = [...arrayOne, element]
+  return arrayOne
+}
+
